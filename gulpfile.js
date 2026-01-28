63e177626453aa89f971c46eb008535c7632ea42
lukedcodes <luked1uk@gmail.com>
2026-01-28 23:43:41 +0000

    Cleanup PHP files and enable Gulp minification

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,27 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const fileinclude = require('gulp-file-include');
+const htmlmin = require('gulp-htmlmin');
 
 // Compile Sass
 gulp.task('sass', function () {
     return gulp.src('scss/*.scss')
-        .pipe(sass())
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
         .pipe(gulp.dest('css'));
 });
 
 // Compile HTML
 gulp.task('fileinclude', function () {
-    return gulp.src(['src/*.html'])
+    return gulp.src(['src/**/*.html', '!src/partials/**'])
         .pipe(fileinclude({
             prefix: '@@',
             basepath: '@file'
+        }))
+        .pipe(htmlmin({
+            collapseWhitespace: true,
+            removeComments: true,
+            minifyJS: true,
+            minifyCSS: true
         }))
         .pipe(gulp.dest('./'));
 });
@@ -24,6 +31,9 @@ gulp.task('watch', function () {
     gulp.watch('scss/*.scss', gulp.series('sass'));
     gulp.watch('src/**/*.html', gulp.series('fileinclude'));
 });
+
+// Build Task (no watch)
+gulp.task('build', gulp.series('sass', 'fileinclude'));
 
 // Default Task
 gulp.task('default', gulp.series('sass', 'fileinclude', 'watch'));
