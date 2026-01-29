80e87faddf2df8d024c9954973245f9d8875dff2
lukedcodes <luked1uk@gmail.com>
2026-01-29 00:24:09 +0000

    Add local dev server (browser-sync) and disable HTML minification

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,17 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const fileinclude = require('gulp-file-include');
 const htmlmin = require('gulp-htmlmin');
+const browserSync = require('browser-sync').create();
 
 // Compile Sass
 gulp.task('sass', function () {
     return gulp.src('scss/*.scss')
         .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-        .pipe(gulp.dest('css'));
+        .pipe(gulp.dest('css'))
+        .pipe(browserSync.stream()); // Inject CSS changes
 });
 
+// Compile HTML
 // Compile HTML
 gulp.task('fileinclude', function () {
     return gulp.src(['src/**/*.html', '!src/partials/**'])
@@ -17,23 +20,23 @@ gulp.task('fileinclude', function () {
             prefix: '@@',
             basepath: '@file'
         }))
-        .pipe(htmlmin({
-            collapseWhitespace: true,
-            removeComments: true,
-            minifyJS: true,
-            minifyCSS: true
-        }))
-        .pipe(gulp.dest('./'));
-});
-
-// Watch Files
-gulp.task('watch', function () {
-    gulp.watch('scss/*.scss', gulp.series('sass'));
-    gulp.watch('src/**/*.html', gulp.series('fileinclude'));
+        .pipe(gulp.dest('./'))
+        .pipe(browserSync.stream()); // Reload on HTML change
 });
 
 // Build Task (no watch)
 gulp.task('build', gulp.series('sass', 'fileinclude'));
 
+// Watch Files & Serve
+gulp.task('watch', function () {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    gulp.watch('scss/*.scss', gulp.series('sass'));
+    gulp.watch('src/**/*.html', gulp.series('fileinclude'));
+});
+
 // Default Task
-gulp.task('default', gulp.series('sass', 'fileinclude', 'watch'));
+gulp.task('default', gulp.series('build', 'watch'));
